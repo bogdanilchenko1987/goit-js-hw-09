db9ad6527a71b8a98e698293b437545b3d1f7638
bogdanilchenko1987 <71671855+bogdanilchenko1987@users.noreply.github.com>
2024-02-16 03:14:23 +0000

    Deploying to gh-pages from @ bogdanilchenko1987/goit-js-hw-09@3afca73b3955342d4eae396838b37b808d7fb038 🚀

--- 01-color-switcher.4901a633.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=null,n={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};n.startBtn.addEventListener("click",(function(){t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),n.startBtn.disabled=!0,n.stopBtn.disabled=!1}),1e3)})),n.stopBtn.addEventListener("click",(function(){clearInterval(t),n.stopBtn.disabled=!0,n.startBtn.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.4901a633.js.map
